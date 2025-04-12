f7d9b9585e7e8ff7e9fa105b58983fe8b445fbe5
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-04-12 21:59:46 +0530

    Add files via upload

---
+++ P2AlphabetTriangle.java
@@ -0,0 +1,12 @@
+public class P2AlphabetTriangle {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            char ch=(char)('A'+i-1);
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
