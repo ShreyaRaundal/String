b71b19fe8b55faf1b2f8e49d3073644bca950f36
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-04-13 09:20:55 +0530

    Add files via upload

---
+++ CaseConversion.java
@@ -0,0 +1,9 @@
+public class CaseConversion {
+    public static void main(String[] args){
+        String str="Hello Shreya";
+
+        System.out.println("LowerCase :"+str.toLowerCase());
+        System.out.println("UpperCase : "+str.toUpperCase());
+
+    }
+}
