a33d734bcace39a09ad4865ec148f0d84c778a8f
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-04-13 16:45:58 +0530

    Add files via upload

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,22 +1,23 @@
 import java.util.HashMap;
 
 public class RomanToInteger {
-  public static void main(String[] args){
-    String roman="XVV";
-    HashMap<Character,Integer>map=new HashMap<>();
-    map.put('I',1);map.put('V',5);map.put('X',10);
-    map.put('L',50);map.put('C',100);
-    map.put('D',100);map.put('M',1000);
+    public static void main(String[] args) {
+        String roman = "XIV";
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1); map.put('V', 5); map.put('X', 10);
+        map.put('L', 50); map.put('C', 100);
+        map.put('D', 500); map.put('M', 1000);
 
-    int result=0;
+        int result = 0;
 
-    for(int i=0;i<roman.length();i++){
-        int curr=map.get(roman.charAt(i));
-        int next=(i+1<roman.length())?map.get(roman.charAt(i+1)):0;
+        for (int i = 0; i < roman.length(); i++) {
+            int curr = map.get(roman.charAt(i));
+            int next = (i + 1 < roman.length()) ? map.get(roman.charAt(i + 1)) : 0;
 
-        if(curr<next)result-=curr;
-        else result += curr;
+            if (curr < next) result -= curr;
+            else result += curr;
+        }
+
+        System.out.println("Integer: " + result);
     }
-    System.out.println("Integer :"+result);
-  }  
 }
