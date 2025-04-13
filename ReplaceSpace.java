1c723890cf5937656caefd60869bd2474b084807
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-04-13 10:08:30 +0530

    Add files via upload

---
+++ ReplaceSpace.java
@@ -0,0 +1,7 @@
+public class ReplaceSpace {
+    public static void main(String[] args){
+        String str="Java is fun";
+        String modified=str.replace(" ","_");
+        System.out.println("Modified: "+modified);
+    }
+}
