ee49a7ad2af5ac788a14e2609e11b8f3eee06f70
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-04-13 17:15:46 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,10 @@
+public class StringCompare {
+    public static void main(String[] args){
+        String a="Shreya",b="Shreya";
+        if(a.equals(b)){
+            System.out.println("String are equals");
+        }else{
+            System.out.println("Not equal");
+        }
+    }
+}
