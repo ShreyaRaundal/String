ef8de0afd3ec0456d4cff3c61052f2c3f670d357
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-04-14 08:13:58 +0530

    Add files via upload

--- CapitalizeWords.java
+++ CapitalizeWords.java
@@ -1,14 +1,14 @@
 public class CapitalizeWords {
-    public static void main(String[] args){
-        String sentence="Java is awesome";
-        String[] words=sentence.split(" ");
+    public static void main(String[] args) {
+        String sentence="java is awesome";
+        String[] words=sentence.split("");
         StringBuilder result=new StringBuilder();
 
-        for(String word:words){
+        for (String word : words) {
             result.append(Character.toUpperCase(word.charAt(0)))
-            .append(word.substring(1)).append(" ");
-
+                  .append(word.substring(1)).append(" ");
         }
-        System.out.println("Capitalized :"+result.toString().trim());
+
+        System.out.println("Capitalized: "+result.toString().trim());
     }
 }
